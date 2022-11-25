50e85be9b6a27d32a4aa0d54af55d844671552cd
Gavinmr1 <gavinriggs28@gmail.com>
2022-11-25 15:43:38 -0800

    remove mini-game and edit portfolio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import tw, { styled } from 'twin.macro'
 import { NavLink } from 'react-router-dom/dist'
 import { SocialIcon } from 'react-social-icons'
 
 export default function NavBar() {
+    const [activeLink, setActiveLink] = useState('home')
+    const [scrolled, setScrolled] = useState(false)
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+        window.addEventListener("scroll", onScroll)
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, [])
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value)
+    }
+
     return (
-        <StyledNavBar>
+        <StyledNavBar scroll={scrolled}>
             <NavWrapper>
-                <Nav to='/' exact className={({ isActive }) => (isActive && "bg-green=500")}>
+                <Nav to='/' exact active={activeLink === 'home'} onClick={() => onUpdateActiveLink('home')}>
                     Gavin Riggs
                 </Nav>
-                <Nav to='/post' className={({ isActive }) => (isActive && "bg-green=500")}>
-                    Blog Posts
-                </Nav>
-                <Nav to='/project' className={({ isActive }) => (isActive && "bg-green=500")}>
+                <Nav to='/projects' active={activeLink === 'projects'} onClick={() => onUpdateActiveLink('projects')}>
                     Projects
                 </Nav>
-                <Nav to='/about' className={({ isActive }) => (isActive && "bg-green=500")}>
+                <Nav to='/about' active={activeLink === 'about'} onClick={() => onUpdateActiveLink('about')}>
                     About Me!
                 </Nav>
             </NavWrapper>
@@ -32,6 +49,7 @@ export default function NavBar() {
 
 const StyledNavBar = styled.header`
 ${tw`absolute w-full bg-blue-900 border-b-2 border-white border-opacity-10 bg-opacity-30 backdrop-blur-[1px] px-10 py-6 flex justify-between mx-auto items-center`}
+${({scrolled}) => scrolled ? tw`` : tw``}
 `
 const NavWrapper = tw.div`flex items-center gap-6`
 const SocialWrapper = tw.div`flex items-center gap-6`
@@ -39,6 +57,6 @@ const SocialWrapper = tw.div`flex items-center gap-6`
 const Nav = styled(NavLink)`
     ${tw`text-white font-semibold text-lg py-2 px-4 hover:text-green-100 hover:bg-blue-900 hover:bg-opacity-30 rounded-lg 
     hover:shadow-lg tracking-widest transition-all ease-in-out duration-300`}
-    ${({isActive}) => isActive && tw`bg-blue-900`}
+    ${({active}) => active && tw`bg-blue-900`}
 `
 const StyledSocialIcon = tw`h-12 w-12`
